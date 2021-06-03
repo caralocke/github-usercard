c8f7b57247429cd36e5eebe4003240bb4dec59cf
caralocke <cara.m.locke@gmail.com>
2021-06-03 10:28:35 -0400

    Completed steps 1 and 2 of index.js. Working on step 3.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+const { default: axios } = require("axios");
+
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.
+get('https://api.github.com/users/caralocke')
+.then(res => {
+  console.log(res.data)
+})
+.catch(err => {
+  console.log(`Here's where you messed up: \n`, err)
+})
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -49,6 +59,35 @@ const followersArray = [];
       </div>
     </div>
 */
+function cardMaker(obj) {
+  const card = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfo = document.createElement('div')
+  const name = document.createElement('h3')
+  const username = document.createElement('p')
+  const location = document.createElement('p')
+  const profile = document.createElement('p')
+  const url = document.createElement('a')
+  const followers = document.createElement('p')
+  const following = document.createElement('p')
+  const bio = document.createElement('p')
+
+  card.appendChild(image)
+  card.appendChild(cardInfo)
+  cardInfo.appendChild(name)
+  cardInfo.appendChild(username)
+  cardInfo.appendChild(location)
+  cardInfo.appendChild(profile)
+  profile.appendChild(url)
+  cardInfo.appendChild(followers)
+  cardInfo.appendChild(following)
+  cardInfo.appendChild(bio)
+
+  card.classList.add('card')
+  cardInfo.classList.add('card-info')
+  name.classList.add('name')
+
+}
 
 /*
   List of LS Instructors Github username's:
